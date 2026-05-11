fb8f59044a0fe19938c316ce7353fe9b7de8ca11
Danush-developer <chitturidanush@gmail.com>
2026-05-11 17:05:20 +0530

    refactor: convert ApplicationRepository to use MongoTemplate

--- FirstMongoproject/src/main/java/com/example/FirstMongoproject/repository/ApplicationRepository.java
+++ FirstMongoproject/src/main/java/com/example/FirstMongoproject/repository/ApplicationRepository.java
@@ -1,18 +1,55 @@
 package com.example.FirstMongoproject.repository;
 
 import com.example.FirstMongoproject.model.JobApplication;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ApplicationRepository extends MongoRepository<JobApplication, String> {
+@Repository
+public class ApplicationRepository {
 
-    Optional<JobApplication> findByJobIdAndEmployeeId(String jobId, String employeeId);
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-    List<JobApplication> findByJobId(String jobId);
+    public JobApplication save(JobApplication application) {
+        return mongoTemplate.save(application);
+    }
 
-    List<JobApplication> findByEmployeeId(String employeeId);
+    public Optional<JobApplication> findById(String id) {
+        return Optional.ofNullable(mongoTemplate.findById(id, JobApplication.class));
+    }
 
-    List<JobApplication> findByStatus(String status);
+    public List<JobApplication> findAll() {
+        return mongoTemplate.findAll(JobApplication.class);
+    }
+
+    public void deleteById(String id) {
+        Query query = new Query(Criteria.where("id").is(id));
+        mongoTemplate.remove(query, JobApplication.class);
+    }
+
+    public Optional<JobApplication> findByJobIdAndEmployeeId(String jobId, String employeeId) {
+        Query query = new Query(Criteria.where("jobId").is(jobId).and("employeeId").is(employeeId));
+        return Optional.ofNullable(mongoTemplate.findOne(query, JobApplication.class));
+    }
+
+    public List<JobApplication> findByJobId(String jobId) {
+        Query query = new Query(Criteria.where("jobId").is(jobId));
+        return mongoTemplate.find(query, JobApplication.class);
+    }
+
+    public List<JobApplication> findByEmployeeId(String employeeId) {
+        Query query = new Query(Criteria.where("employeeId").is(employeeId));
+        return mongoTemplate.find(query, JobApplication.class);
+    }
+
+    public List<JobApplication> findByStatus(String status) {
+        Query query = new Query(Criteria.where("status").is(status));
+        return mongoTemplate.find(query, JobApplication.class);
+    }
 }
